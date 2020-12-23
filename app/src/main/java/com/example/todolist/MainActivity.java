2551bb67372a3ace2bf28d6769dd1812b0457c88
rPy3oFF <fred.reshetov@gmail.com>
2020-12-23 18:25:38 +0200

    Commnet from Freddy Method From Freddy

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fredy();
+
+        //Hello From Freddy
+
+    }
+
+    private void fredy(){
+
     }
 }
