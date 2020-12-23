fef2c6a17b875d3c93ffa07e0fe7307d87492c9f
arieledri25 <opoae20@gmail.com>
2020-12-23 18:25:40 +0200

    פונקציה של אריאל

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arielEdri();
+    }
+
+    private void arielEdri(){
+        Toast.makeText(this,"ariel",Toast.LENGTH_LONG).show();
     }
 }
